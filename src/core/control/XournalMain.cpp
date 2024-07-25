81763f91caa9e0746c2a3b668a06d38996ef20de
Thomas Moerschell <144177788+tmoerschell@users.noreply.github.com>
2024-07-25 02:37:48 +0200

    Throw out of the LoadHandler on exception

--- src/core/control/XournalMain.cpp
+++ src/core/control/XournalMain.cpp
@@ -226,6 +226,18 @@ void exitOnMissingPdfFileName(const LoadHandler& loader) {
         exit(-2);
     }
 }
+std::unique_ptr<Document> loadDocumentOrExit(const char* filename) {
+    std::unique_ptr<Document> doc{};
+    try {
+        LoadHandler loader;
+        doc = loader.loadDocument(filename);
+
+        exitOnMissingPdfFileName(loader);
+    } catch (std::exception& e) {
+        g_error("Document was not loaded: %s", e.what());
+    }
+    return std::move(doc);
+}
 }  // namespace
 
 
@@ -248,14 +260,7 @@ void exitOnMissingPdfFileName(const LoadHandler& loader) {
  */
 auto exportImg(const char* input, const char* output, const char* range, const char* layerRange, int pngDpi,
                int pngWidth, int pngHeight, ExportBackgroundType exportBackground) -> int {
-    LoadHandler loader;
-    auto doc = loader.loadDocument(input);
-    if (doc == nullptr) {
-        g_error("Document was not loaded: %s", loader.getErrorMessages().c_str());
-    }
-
-    exitOnMissingPdfFileName(loader);
-
+    std::unique_ptr<Document> doc = loadDocumentOrExit(input);
     return ExportHelper::exportImg(doc.get(), output, range, layerRange, pngDpi, pngWidth, pngHeight, exportBackground);
 }
 
@@ -302,14 +307,7 @@ auto saveDoc(const char* input, const char* output) -> int {
  */
 auto exportPdf(const char* input, const char* output, const char* range, const char* layerRange,
                ExportBackgroundType exportBackground, bool progressiveMode) -> int {
-    LoadHandler loader;
-    auto doc = loader.loadDocument(input);
-    if (doc == nullptr) {
-        g_error("Document was not loaded: %s", loader.getErrorMessages().c_str());
-    }
-
-    exitOnMissingPdfFileName(loader);
-
+    std::unique_ptr<Document> doc = loadDocumentOrExit(input);
     return ExportHelper::exportPdf(doc.get(), output, range, layerRange, exportBackground, progressiveMode);
 }
 
